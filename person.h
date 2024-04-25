3aeea754300882b0c3ef53ff9aab50e3b0b4fc4b
elenachau <elenaxcha@gmail.com>
2024-04-25 13:47:55 -0700

    create person.h

---
+++ person.h
@@ -0,0 +1,33 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include "appointment.h"
+#include "dynamicArray.h"
+
+class Person{
+    protected: //attributes accessible within subclasses
+        string first, last, username, password;
+        DynamicArray<Appointment> bookedAppointments;
+
+    public:
+        Person();
+        Person(string, string, string, string, DynamicArray<Appointment>);
+        Person(const Person&);
+
+        string getFirst();
+        string getLast();
+        string getUsername();
+        string getPassword();
+        DynamicArray<Appointment> getBookedAppointment();
+
+        void setFirst(string);
+        void setLast(string);
+        void setUsername(string);
+        void setPassword(string);
+        void addBooking(Appointment);
+        void removeBooking(int i);
+
+        Person& operator=(const Person&);
+};
+
+#endif
